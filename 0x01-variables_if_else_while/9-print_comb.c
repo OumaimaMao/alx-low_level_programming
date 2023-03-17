6b34dbab175c7690ceb632e3ce0da7963df7182f
Oumaima Mao <mao.oumaima@gmail.com>
2023-03-17 11:22:47 -0700

    Task 9 done

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,13 +9,15 @@ int main(void)
 {
 	int i;
 
-	for (i = 0; i <= 8; i++)
+	for (i = 0; i <= 9; i++)
 	{
 		putchar(i + '0');
+		if (i != 9)
+		{
 		putchar(',');
 		putchar(' ');
+		}
 	}
-		putchar('9');
 	putchar('\n');
 	return (0);
 }
