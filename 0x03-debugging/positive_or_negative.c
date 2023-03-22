300906f565ba8633f683567885ccffcde16f4d90
Oumaima Mao <mao.oumaima@gmail.com>
2023-03-22 16:56:34 -0700

    Task 0 done

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * positive_or_negative - cheks for positive or negative number
+ * @i: the number
+ * Return: 0
+ */
+void positive_or_negative(int i)
+{
+	if (i < 0)
+		printf("%d is negative\n", i);
+	else
+		printf("%d is positive\n", i);
+}
